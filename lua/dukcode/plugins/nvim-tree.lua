e73a5eea78952bdb27cd63cb0af760317be61f59
김덕윤 <duk9741@gmail.com>
2024-10-25 11:35:09 +0900

    feat: add auto-close at nvim-tree

--- lua/dukcode/plugins/nvim-tree.lua
+++ lua/dukcode/plugins/nvim-tree.lua
@@ -8,6 +8,16 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
+    -- auto close
+    vim.api.nvim_create_autocmd("BufEnter", {
+      nested = true,
+      callback = function()
+        if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+          vim.cmd "quit"
+        end
+      end
+    })
+
     nvimtree.setup({
       view = {
         width = 35,

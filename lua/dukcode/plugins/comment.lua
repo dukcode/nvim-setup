14dcad2cb0a30c5ec3c27a1fb5e3a59da3f2f9b7
dukcode <duk9741@gmail.com>
2026-04-16 15:41:24 +0900

    fix: ts_context_commentstring CursorHold autocmd 에러 수정
    enable_autocmd = false로 설정하여 treesitter parser가 없는
    버퍼에서 language_tree nil 에러가 발생하는 것을 방지

--- lua/dukcode/plugins/comment.lua
+++ lua/dukcode/plugins/comment.lua
@@ -5,15 +5,17 @@ return {
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
+    -- CursorHold autocmd를 비활성화하여 treesitter parser가 없는 버퍼에서
+    -- language_tree nil 에러가 발생하는 것을 방지한다.
+    -- 대신 Comment.nvim의 pre_hook을 통해 주석 시점에만 commentstring을 계산한다.
+    require("ts_context_commentstring").setup({
+      enable_autocmd = false,
+    })
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
+    -- pre_hook: 주석 토글 시 ts_context_commentstring을 호출하여
+    -- tsx, jsx, svelte, html 등 embedded language의 commentstring을 자동 결정한다.
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
     })
   end,
 }
